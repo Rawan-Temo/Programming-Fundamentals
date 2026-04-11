cf8c61774d59c85176bfbe6665f7a860a37547f2
rawan-temo <rinas1122k@gmail.com>
2026-04-11 16:25:32 +0300

    feat: add 2 pages for how to exec cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int x = 10;
+
+    int  y = 3;
+
+
+
+    //with print a float from two integers we need to cast one of them to float or double
+    cout << "The result of x / y is: " << (double)x / y << endl; // or (double)x / y
+    
+
+
+    
+    return 0;
+}
